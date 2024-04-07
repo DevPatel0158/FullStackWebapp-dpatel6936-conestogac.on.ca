c56211249a58b8d633dafb5299471160ef522be4
DEVS0158 <dpatel6936@conestogac.on.ca>
2024-04-06 22:11:09 -0400

    in models py code created a profile class in which added variour user's traits like usr, bio, avatar, updated, created

--- Scripts/src/posts/models.py
+++ Scripts/src/posts/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from profiles.models import Profile
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField((blank=True))
-    avatar = models.ImageField(default='avatar.png', upload_to='avatars')
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True)
+
+    author = models.ForeignKey(Profile, on_delete=models. CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
-        return f"profile of the user {self.user.username}"
+        return str(self.title)
